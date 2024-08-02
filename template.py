32fca573e88f8f4261d2ee90686e00397c25a977
Aijaz <aijaz523@gmail.com>
2024-08-02 12:49:28 +0530

    Model code added

--- template.py
+++ template.py
@@ -10,9 +10,12 @@ list_of_files=[
     f"src/{package_name}/components/data_ingestion.py",
     f"src/{package_name}/components/data_transformation.py",
     f"src/{package_name}/components/model_trainer.py",
+    f"src/{package_name}/components/model_evaluation.py",
+
     f"src/{package_name}/pipelines/__init__.py",
     f"src/{package_name}/pipelines/training_pipeline.py",
     f"src/{package_name}/pipelines/prediction_pipeline.py",
+    
     f"src/{package_name}/logger.py",
     f"src/{package_name}/exception.py",
     f"src/{package_name}/utils/__init__.py",
